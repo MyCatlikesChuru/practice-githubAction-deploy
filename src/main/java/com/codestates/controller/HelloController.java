ef4bd5a5380ed368b80d71b651e2bdfd71817528
Lee JaeHyeok <dhfif718@gmail.com>
2022-12-08 15:09:40 +0900

    controller

--- src/main/java/com/codestates/controller/HelloController.java
+++ src/main/java/com/codestates/controller/HelloController.java
@@ -5,9 +5,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/")
 public class HelloController {
-    @GetMapping
+    @GetMapping("/")
     public String getHello(){
         return "Hello World! Hello BE BootCamp";
     }
