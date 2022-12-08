7b249bbbd2ea6fda782517a53f7255167cd54403
Lee JaeHyeok <dhfif718@gmail.com>
2022-12-08 15:27:54 +0900

    log 테스트

--- src/main/java/com/codestates/practicegithubActiondeploy/controller/HelloController.java
+++ src/main/java/com/codestates/practicegithubActiondeploy/controller/HelloController.java
@@ -1,13 +1,16 @@
 package com.codestates.practicegithubActiondeploy.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class HelloController {
     @GetMapping("/")
     public String hello(){
+        log.info("Hello Controller 출력완료 !!");
         return "Hello World! Hello BE BootCamp";
     }
 }
